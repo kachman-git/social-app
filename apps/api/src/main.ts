0aec72014fa0789ef365ad9916e2c14e8392da3a
kachman-git <onyekachiokpala67@gmail.com>
2025-01-02 10:40:38 +0000

    Cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'https://social-app-production-c882.up.railway.app',
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
   app.useGlobalPipes(
